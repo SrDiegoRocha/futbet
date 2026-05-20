a62d3995a1a6c6c8f9eda3a5f30c635e4d2d8a5c
Diego Rocha <diego.rb0307@gmail.com>
2026-05-20 20:26:31 -0300

    initial commit

---
+++ src/main/java/com/example/futbet/FutbetApplication.java
@@ -0,0 +1,13 @@
+package com.example.futbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutbetApplication.class, args);
+    }
+
+}
